674d1f9056850c20a0a063abde9f33df8fd80410
Artem Sokolov <artem.sokolov@gmail.com>
2021-01-27 22:44:20 -0500

    Downloading PanCan33 data

--- get-data.R
+++ get-data.R
@@ -10,3 +10,7 @@ synGet( "syn2701943", downloadLocation="./data/PCBC",
 ## Download PCBC annotations
 qry <- synTableQuery( "SELECT UID, Diffname_short FROM syn3156503" )
 file.copy( qry$filepath, "./data/PCBC/meta.csv" )
+
+## Download PAnCan33 data
+synGet( "syn4976369", downloadLocation="./data/PanCan33",
+       ifcollision="overwrite.local" )
